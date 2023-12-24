b67753176b23438eb768b2d8ecc8f33806090920
Kevin <kevingarrido711@gmail.com>
2023-12-23 23:01:08 -0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Navbar from '@/src/app/components/Navbar'
+import { Nabla } from 'next/font/google'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Navbar/>
+  )
+}
